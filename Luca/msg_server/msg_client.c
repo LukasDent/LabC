359c8991bbac31ef76eef8e77ff8b8ebb59171ee
luca <luca@localhost.localdomain>
2013-12-09 12:21:55 +0100

    Signed-off-by: luca <luca@localhost.localdomain>
    	modificato:   Luca/msg_server/msg_client.c
    	modificato:   Luca/msg_server/msg_server.c

--- Luca/msg_server/msg_client.c
+++ Luca/msg_server/msg_client.c
@@ -25,29 +25,60 @@ int main(int argc, char** argv) {
   int clientId;
   struct requestMsg richiesta;;
   struct responseMsg risposta;
+  
+  
+  /*----------------------------------------------------*/
   /*Cerco di collegarmi alla coda di messaggi del server*/
+  /*----------------------------------------------------*/
+  
    if((serverId = msgget(KEY,0660))<0){
-     /*Se non riesce stampo l'errore MSGGET*/
-   printf("MSGGET"); 
-   exit(1);
- }
+	printf("Errore di connesione al server \t%s\n",strerror(errno)); 
+	exit(1);
+    }
+ /* ---------------------------------------------------------*/ 
  /*Sono connesso alla coda messaggi, setto la struttura dati*/
- printf("%d\n",serverId);
+ /* --------------------------------------------------------*/
+ 
   richiesta.mtype=1;
   richiesta.clientId=getpid();
   strcpy(richiesta.pathname,"file");
+  
+  /*------------------------------------------*/
   /*Invio la richiesta al server con la MSGSND*/
+  /*------------------------------------------*/
+  
  if((msgsnd(serverId,&richiesta ,sizeof(richiesta),IPC_NOWAIT))<0){
-    printf("%s",strerror(errno));
+    printf("Errore invio richiesta al server \t%s\n",strerror(errno));
     exit(1);
  }
+ 
+ /*-----------------------------------------------------------*/
  /*Creo la coda messaggi per la risposta e mi metto in ascolto*/
- if((clientId=msgget(getpid(),0660|IPC_CREAT))<0)
-   printf("MSGGET2");
- while(strcmp("END_MESSAGE_STRING",risposta.data)!=0){
- if((msgrcv(clientId,&risposta,sizeof(risposta),0,0 ))<0){
-   printf("%s",strerror(errno));
+ /*-----------------------------------------------------------*/
+ 
+ if((clientId=msgget(getpid(),0660|IPC_CREAT))<0){
+   printf("Errore creazione coda messaggi client\t%s\n",strerror(errno));
+   exit(1);
  }
- printf(risposta.data);
+ 
+ /*----------------------------------------------------------------------------*/
+ /*Leggo la risposta del server finchè non ricevo il segnale di fine messaggio*/
+ /*--------------------------------------------------------------------------*/
+ 
+ while(strcmp("END_MESSAGE_STRING",risposta.data)!=0){
+   
+      /*-----------------------------------------*/
+      /*Se non riesco a leggere termino il client*/
+      /*-----------------------------------------*/
+      
+      if((msgrcv(clientId,&risposta,sizeof(risposta),0,0 ))<0){
+	  printf("Errore ricezione messaggio dal server \t%s\n",strerror(errno));
+	}
+	
+      /*------------------------------------*/
+      /*Stampo a video la risposta ottenuta */
+      /*------------------------------------*/
+      
+      printf("%s\n",risposta.data);
  }
 }
--- Luca/msg_server/msg_server.c
+++ Luca/msg_server/msg_server.c
@@ -22,52 +22,115 @@ struct requestMsg { // Richiesta (client --> server)
 
 
 int main(int argc, char** argv) {
+  
  int serverId;
  struct requestMsg richiesta;
  struct responseMsg risposta;
  int clientId;
-FILE *fp;
- // creazione della coda server server message queue!
+ FILE *fp;
+ 
+ 
+ /*---------------------------------*/
+ /*Creo la coda messaggi del server*/
+ /* ------------------------------*/
+ 
  if((serverId = msgget(KEY,IPC_CREAT|0660))==-1){
-   printf("MSGGET");
+   printf("Errore msgget server \t%s\n",strerror(errno));
    exit(1);
  }
- printf("%d\n",serverId);
- // lettura richieste e gestione di ciascuna !
- // in un processo figlio separato !
+ 
+ 
+ 
+ /*-----------------------------------------------------*/
+ /*Entro in loop per attendere le richieste dei client */
+ /* --------------------------------------------------*/
+ 
+
  for(;;) {
-  printf("Attesa msg\n");
- switch((msgrcv(serverId,&richiesta,sizeof(richiesta),0,0 ))){ 
-case -1: //printf("Errore nella gestione del messaggio");
-	  break;
- default: 
-	  switch(fork()){
-	    case -1: printf("Errore nella generazione del figlio");
-		      break;
-	    case 0:	      
-		     /*Mi collego alal coda messaggi del client*/
-		      clientId=msgget(richiesta.clientId,0660);
-		     /*apro il file richiesto in lettura e leggo il suo contenuto*/
-		     
-		     fp=fopen(richiesta.pathname,"r");
-		     if(!fp){
-		       printf("Impossibile aprire il file richiesto dal client in lettura");
-			break;}
-			int ct=1;
-			while(!feof(fp)){
-			  fgets(risposta.data,100,fp);
-			  /*invio il contenuto del file al client*/
-			  if((msgsnd(clientId,&risposta,sizeof(risposta),IPC_NOWAIT))==-1)
-			      printf("%s\n",strerror(errno));
-			      printf("Invio n%d\n",ct);
-			      ct++;
-			}
-			strcpy(risposta.data,"END_MESSAGE_STRING");
-			msgsnd(clientId,&risposta,sizeof(risposta),IPC_NOWAIT);
-			    break;
+   
+    printf("Attesa msg\n");
+    switch((msgrcv(serverId,&richiesta,sizeof(richiesta),0,0 ))){ 
+	  case -1: printf("Errore msgrcv server\t%s\n",strerror(errno));
+		   break;
+		   
+	  default: 
+	    
+		  /*------------------------------------------------*/
+		  /*Genero il figlio per la gestione della richiesta*/
+		  /*------------------------------------------------*/
+		  
+		  switch(fork()){
 		    
-	    default:	break;
+		    case -1: printf("Errore Fork:\t%s\n",strerror(errno));
+					   break;
+				  
+				  case 0: /*----------------------------------------*/	      
+					  /*Mi collego alal coda messaggi del client*/
+					  /*----------------------------------------*/
+					  
+					  /*-----------------------------------------------------------------------------*/
+					  /*Se fallisce la connesione alla cosa messaggi del client annullo la richiesta */
+					  /*----------------------------------------------------------------------------*/
+					  
+					  if((clientId=msgget(richiesta.clientId,0660))==-1){
+					      printf("Errore msgget client \t%s\n",strerror(errno));
+					      break;
+					  }
+					  
+					  
+					  /*----------------------------------------------------------*/
+					  /*apro il file richiesto in lettura e leggo il suo contenuto*/
+					  /*----------------------------------------------------------*/
+					  
+					  fp=fopen(richiesta.pathname,"r");
+					  
+					  /*----------------------------------------------------------------*/
+					  /*Se fallisce l'apertura del file annullo la richiesta del client */
+					  /*---------------------------------------------------------------*/
+					  
+					  if(!fp){
+						  printf("Errore fopen \t%s\n",strerror(errno));
+						   break;
+					  }
+					  
+					  /*-----------------------------------------------*/
+					  /*Inizio la lettura del file richiesto dal client*/
+					  /*-----------------------------------------------*/
+					  
+					  int ct=1;/*contatore per indicare numero di letture effettuate sul file*/
+					  
+					   while(!feof(fp)){
+					     
+					    /*------------------------------------------------------------------------------------------------------*/
+					    /* Prendo in lettura con la fgets tanti caratteri quando è grande il campo data della struttura risposta*/
+					    /*------------------------------------------------------------------------------------------------------*/
+					    
+					    fgets(risposta.data,100,fp);
+					    
+					    /*-------------------------------------*/
+					    /*invio il contenuto del file al client*/
+					    /*-------------------------------------*/
+					    
+					    if((msgsnd(clientId,&risposta,sizeof(risposta),IPC_NOWAIT))==-1)
+					      printf("Errore msgsnd client \t%s\n",strerror(errno));
+					    
+					    
+					    printf("Invio numero:\t%d\n",ct);
+						  ct++;
+					  
+					     
+					  }
+					  /*-------------------------------------------*/
+					  /*Invio il messagio di fine lettura al client*/
+					  /*-------------------------------------------*/
+					  strcpy(risposta.data,"END_MESSAGE_STRING");
+					  if(msgsnd(clientId,&risposta,sizeof(risposta),IPC_NOWAIT)==-1)
+					    printf("Errore invio END_STRING \t%s\n",strerror(errno));
+					  break;
+		    
+	    default:	/*Padre si rimette in attesa di altre richieste*/ break;
 	  }
  }
  }
 }
+
